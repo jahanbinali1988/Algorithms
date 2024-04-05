e26cd5e099342f9271abbabe1e01f115cf9bea90
Ali Akbar Jahanbin <jahanbin.ali1988@hotmail.com>
2024-04-06 02:39:09 +0330

    Add project files.

---
+++ Algorithms.Implementation/SelectionSort.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms.Implementation
+{
+	/// <summary>
+	/// the aim of this algorithm is finding the minimum value of unsorted list and put in the right place of the listss
+	/// </summary>
+	public class SelectionSort
+	{
+		/// <summary>	
+		/// We devide a list of number into two parts, sorted and unsorted. 
+		/// At each steps we find smallest number from unsorted part and move it to the end of sorted part. 
+		/// We continue this process until all numbers in the unsorted part move to sorted part.
+		/// </summary>
+		/// <param name="givenArray">The array to sort</param>
+		/// <returns></returns>
+		public int[] Sort(int[] givenArray)
+		{
+			for (int i = 0; i < givenArray.Length; i++)
+			{
+				var smallestNumber = givenArray[i];
+
+				for (int j = givenArray.Length -1; j > i ; j--)
+				{
+					var comparedNumber = givenArray[j];
+					if (smallestNumber > comparedNumber)
+					{
+						givenArray[i] = givenArray[j];
+						givenArray[j] = smallestNumber;
+					}
+				}
+			}
+
+			return givenArray;
+		}
+	}
+}
---
+++ Algorithms.Tests/SelectionSortTests.cs
@@ -0,0 +1,29 @@
+using Algorithms.Implementation;
+
+namespace Algorithms.Tests
+{
+	public class SelectionSortTests
+	{
+		[Fact]
+		public void Sort_returing_array_has_the_same_Size()
+		{
+			var sort = new SelectionSort();
+			var unsortedArray = new int[] { 1, 2 };
+
+			var sortedArray = sort.Sort(unsortedArray);
+
+			Assert.Equal(unsortedArray.Length, sortedArray.Length);
+		}
+
+		[Fact]
+		public void Sort_works_correctly()
+		{
+			var sort = new SelectionSort();
+			var unsortedArray = new int[] { 2, 1, 4, 3 };
+
+			var sortedArray = sort.Sort(unsortedArray);
+
+			Assert.Equivalent(unsortedArray.Order(), sortedArray);
+		}
+	}
+}
