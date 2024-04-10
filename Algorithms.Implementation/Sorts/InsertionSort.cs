8495ab1e93940b1edf0a750d7407290fa1b43a89
Ali Akbar Jahanbin <jahanbin.ali1988@hotmail.com>
2024-04-10 23:40:16 +0330

    implement quick sort

---
+++ Algorithms.Implementation/Sorts/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementation.Sorts
+{
+    public class InsertionSort
+    {
+        public int[] Sort(int[] givenArray)
+        {
+            for (int i = 1; i < givenArray.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (givenArray[j] < givenArray[j - 1])
+                    {
+                        int temp = givenArray[j - 1];
+                        givenArray[j - 1] = givenArray[j];
+                        givenArray[j] = temp;
+                    }
+                }
+            }
+            return givenArray;
+        }
+    }
+}
---
+++ Algorithms.Implementation/Sorts/QuickSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementation.Sorts
+{
+	public class QuickSort
+	{
+		public void Sort(int[] arr, int low, int high)
+		{
+			if (low < high)
+			{
+				// Partitioning index, arr[pivotIndex] is now at the correct position
+				int pivotIndex = Partition(arr, low, high);
+
+				// Recursively sort elements before and after the partition
+				Sort(arr, low, pivotIndex - 1);
+				Sort(arr, pivotIndex + 1, high);
+			}
+		}
+
+		public int Partition(int[] arr, int low, int high)
+		{
+			// Choose the rightmost element as the pivot
+			int pivot = arr[high];
+			int i = low - 1; // Index of smaller element
+
+			for (int j = low; j < high; j++)
+			{
+				// If current element is smaller than or equal to pivot
+				if (arr[j] <= pivot)
+				{
+					i++;
+					// Swap arr[i] and arr[j]
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+
+			// Swap arr[i+1] and arr[high] (or pivot)
+			int temp2 = arr[i + 1];
+			arr[i + 1] = arr[high];
+			arr[high] = temp2;
+
+			return i + 1;
+		}
+	}
+}
